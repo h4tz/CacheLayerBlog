c16ac33093024834a98815752a4b9c61dd9af623
h4tz <blizerd.777@gmail.com>
2025-07-03 18:27:11 +0000

    implemented caching

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .models import Post
 from .serializers import PostSerializer
 from django.views import View
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 
 
 class PostListAPIView(ListAPIView):
@@ -15,11 +17,12 @@ class PostDetailAPIView(RetrieveAPIView):
     serializer_class = PostSerializer
     lookup_field = 'slug'
     
-    
+@method_decorator(cache_page(15), name='dispatch')
 class PostListView(View):
     def get(self, request):
         return render(request, "blog/post_list.html")
 
+@method_decorator(cache_page(60 * 5), name='dispatch')
 class PostDetailView(View):
     def get(self, request, slug):
         return render(request, "blog/post_detail.html", {"slug": slug})

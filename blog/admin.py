d1f3b6bd147989d34b3ed659cb6883866868e970
h4tz <blizerd.777@gmail.com>
2026-09-13 18:39:47 +0000

    fix: register blog admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "created_at")
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ("title",)
+    ordering = ("-created_at",)
